c52b0e3ee832352e00260b36bced7ad088c4a23d
umer-farooq <Umer.Farooq@huddle.com>
2016-09-21 22:27:40 +0100

    Converted docs to sphinx

--- conf.py
+++ conf.py
@@ -1,7 +1,36 @@
-from recommonmark.parser import CommonMarkParser
+# -*- coding: utf-8 -*-
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+import sys, os
 
-source_suffix = ['.rst', '.md']
+sys.path.insert(0, os.path.abspath('extensions'))
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+              'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig',
+              'epub2', 'mobi', 'autoimage', 'code_example']
+
+todo_include_todos = True
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = []
+add_function_parentheses = True
+#add_module_names = True
+# A list of ignored prefixes for module index sorting.
+#modindex_common_prefix = []
+
+project = u'Music for Geeks and Nerds'
+copyright = u'2012, Pedro Kroger'
+
+version = ''
+release = ''
+
+################################################################################
+
+
+def setup(app):
+     from sphinx.util.texescape import tex_replacements
+     tex_replacements += [(u'♮', u'$\\natural$'),
+                          (u'ē', u'\=e'),
+                          (u'♩', u'\quarternote'),
+                          (u'↑', u'$\\uparrow$'),
+                          ]
---
+++ docs/resources/meaning-context.rst
@@ -0,0 +1,2 @@
+Meaning Context
+===============

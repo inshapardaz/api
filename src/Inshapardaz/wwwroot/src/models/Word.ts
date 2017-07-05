785357a735df37676aa46c99c8da3856d6ff2642
Muhammad Farooq <muhammad.farooq@just-eat.com>
2017-07-05 19:45:18 +0100

    Added ui

---
+++ src/Inshapardaz/wwwroot/src/models/Word.ts
@@ -0,0 +1,13 @@
+export class Word {
+    public id: number;
+    public title: string;
+    public titleWithMovements: string;
+    public pronunciation : string;
+    public description: string;
+    
+    public selfLink : string;
+    public relationsLink : string;
+    public detailsLink : string;
+    public updateLink :string;
+    public deleteLink : string;
+}

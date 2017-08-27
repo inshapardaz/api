5bb1ec575e452fbf836162a1023c255c2a1140a4
Muhammad Farooq <muhammad.farooq@just-eat.com>
2017-08-27 08:53:38 +0100

    Optimised output

--- src/Inshapardaz/wwwroot/config/webpack.prod.js
+++ src/Inshapardaz/wwwroot/config/webpack.prod.js
@@ -1,14 +1,12 @@
 var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const path = require('path');
 const rootDir = path.resolve(__dirname, '..');
 
 module.exports = webpackMerge(commonConfig, {
-
-devtool: 'cheap-module-eval-source-map',
-
 output: {
     path: path.resolve(rootDir, ''),
     filename: '[name].js',
@@ -16,11 +14,7 @@ output: {
     },
 
     plugins: [
-        new ExtractTextPlugin('[name].css')
-    ],
-
-    devServer: {
-        historyApiFallback: true,
-        stats: 'minimal'
-    }
+        new ExtractTextPlugin('[name].css'),
+        new UglifyJSPlugin()
+    ]
 });

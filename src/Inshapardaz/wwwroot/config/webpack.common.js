28d2d00ffcfba6f80b07415d4b92264f90816027
Muhammad Farooq <muhammad.farooq@just-eat.com>
2017-10-04 23:41:23 +0100

    Fixed html-loader issue where it was corrupting angular tags and updated to latest angular

--- src/Inshapardaz/wwwroot/config/webpack.common.js
+++ src/Inshapardaz/wwwroot/config/webpack.common.js
@@ -1,7 +1,6 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const path = require('path');
@@ -28,22 +27,29 @@ module.exports = {
                 loaders: ['awesome-typescript-loader', 'angular2-template-loader']
             }, {
                 test: /\.html$/,
-                loader: 'html-loader'
+                loader: 'html-loader',
+                options: {
+                    minimize: true,
+                    removeComments: true,
+                    collapseWhitespace: true,
+              
+                    // angular 2 templates break if these are omitted
+                    removeAttributeQuotes: false,
+                    keepClosingSlash: true,
+                    caseSensitive: true,
+                    conservativeCollapse: true,
+                  }              
             }, {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
                 loader: 'file-loader?name=assets/[name].[hash].[ext]'
             }, {
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                loaders: ['raw-loader', 'sass-loader'] // sass-loader not scss-loader
+                loaders: ['raw-loader', 'sass-loader']
             }, {
                 test: /\.css$/,
-                //loaders: ['to-string-loader', 'css-loader']
                 use: [ 'style-loader', 'css-loader' ]
             }
-            //,{
-            //     test: /[\/\\]node_modules[\/\\]some-module[\/\\]index\.js$/,
-            //     loader: "imports-loader?this=>window"}
         ]
     },
 
@@ -61,7 +67,6 @@ module.exports = {
             from: 'src/i18n',
             to: './assets/i18n'
         }]),
-        //new FaviconsWebpackPlugin('./src/assets/img/favicons/favicon.png'),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',

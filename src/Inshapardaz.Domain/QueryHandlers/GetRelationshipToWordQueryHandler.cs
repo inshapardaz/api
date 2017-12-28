706350c2be711ba2bc384226f46e0e0a18d792e0
Muhammad Farooq <muhammad.farooq@just-eat.com>
2017-12-28 09:58:23 +0000

    Added support to remove the relations before the word is deleted. Due to multiple relationships to same table, it is not possible to have two cascasde delete statements

---
+++ src/Inshapardaz.Domain/QueryHandlers/GetRelationshipToWordQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Paramore.Darker;
+using Inshapardaz.Domain.Queries;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Inshapardaz.Domain.Database;
+using Inshapardaz.Domain.Database.Entities;
+
+namespace Inshapardaz.Domain.QueryHandlers
+{
+    public class GetRelationshipToWordQueryHandler : QueryHandlerAsync<GetRelationshipToWordQuery,
+        IEnumerable<WordRelation>>
+    {
+        private readonly IDatabaseContext _database;
+
+        public GetRelationshipToWordQueryHandler(IDatabaseContext database)
+        {
+            _database = database;
+        }
+
+        public override async Task<IEnumerable<WordRelation>> ExecuteAsync(GetRelationshipToWordQuery query,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await _database.WordRelation
+                .Include(r => r.RelatedWord)
+                .Include(r => r.SourceWord)
+                .Where(t => t.RelatedWordId == query.WordId)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}

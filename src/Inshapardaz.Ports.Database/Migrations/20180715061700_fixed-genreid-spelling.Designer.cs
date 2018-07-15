3f59766c63ff5c5c7003f79a7f33db69998b4ae0
muhammad-farooq <muhammad.farooq@just-eat.com>
2018-07-15 07:17:52 +0100

    Fixed the GenreId column name

--- src/Inshapardaz.Ports.Database/Migrations/20180715061700_fixed-genreid-spelling.Designer.cs
+++ src/Inshapardaz.Ports.Database/Migrations/20180715061700_fixed-genreid-spelling.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace Inshapardaz.Ports.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180715061700_fixed-genreid-spelling")]
+    partial class fixedgenreidspelling
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

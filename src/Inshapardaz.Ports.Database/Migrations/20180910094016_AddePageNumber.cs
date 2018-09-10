ae8d570b6748945d67db915d60befcaff291094d
muahmmad-farooq <muahmmad.farooq@just-eat.com>
2018-09-10 10:42:02 +0100

    Added Page number

---
+++ src/Inshapardaz.Ports.Database/Migrations/20180910094016_AddePageNumber.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Inshapardaz.Ports.Database.Migrations
+{
+    public partial class AddePageNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PageNumber",
+                schema: "Library",
+                table: "BookPage",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PageNumber",
+                schema: "Library",
+                table: "BookPage");
+        }
+    }
+}

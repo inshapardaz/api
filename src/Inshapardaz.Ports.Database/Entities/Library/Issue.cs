5ba1de67a9ac4db90db0689c37eca18dbc9e868a
Muhammad Farooq <muhammad.farooq@just-eat.com>
2019-03-13 17:21:30 +0000

    Added magazine issue

---
+++ src/Inshapardaz.Ports.Database/Entities/Library/Issue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inshapardaz.Ports.Database.Entities.Library
+{
+    public class Issue
+    {
+        public int Id { get; set; }
+
+        public int IssueNumber { get; set; }
+
+        public int VolumeNumber { get; set; }
+
+        public DateTime IssueDate { get; set; }
+
+        public int? ImageId { get; set; }
+
+        public int MagazineId { get; set; }
+
+        public virtual Magazine Magazine { get; set; }
+
+    }
+}
--- src/Inshapardaz.Ports.Database/Migrations/20190313172038_AddedMagazineIssue.Designer.cs
+++ src/Inshapardaz.Ports.Database/Migrations/20190313172038_AddedMagazineIssue.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace Inshapardaz.Ports.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190313172038_AddedMagazineIssue")]
+    partial class AddedMagazineIssue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

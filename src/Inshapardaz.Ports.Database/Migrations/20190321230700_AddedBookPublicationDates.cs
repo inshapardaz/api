6ccaa0daad72ab0478b66274559963d0d013cef7
Muhammad Farooq <muhammad.farooq@just-eat.com>
2019-03-21 23:08:39 +0000

    Added missing migrations

---
+++ src/Inshapardaz.Ports.Database/Migrations/20190321230700_AddedBookPublicationDates.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Inshapardaz.Ports.Database.Migrations
+{
+    public partial class AddedBookPublicationDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Copyrights",
+                schema: "Library",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "YearPublished",
+                schema: "Library",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Copyrights",
+                schema: "Library",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "YearPublished",
+                schema: "Library",
+                table: "Book");
+        }
+    }
+}

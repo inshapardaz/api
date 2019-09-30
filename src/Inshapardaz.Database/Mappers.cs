8184077700e3cf5eebff195abbe71a56fbfb413b
Omar Faruk <omar.faruk@outlook.com>
2019-09-30 23:35:55 +0100

    Added first dapper repository

---
+++ src/Inshapardaz.Database/Mappers.cs
@@ -0,0 +1,23 @@
+﻿using Inshapardaz.Database.Dto.Library;
+using Inshapardaz.Domain.Entities;
+using Inshapardaz.Domain.Entities.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inshapardaz.Database
+{
+    public static class Mappers
+    {
+        public static Page<Author> Map(this IEnumerable<AuthorDto> source, int pageNumber, int pageSize, int totalCount) => 
+              new Page<Author>
+              {
+                  Data = source.Select(Map),
+                  PageNumber = pageNumber,
+                  PageSize = pageSize,
+                  TotalCount = totalCount
+              };
+        public static Author Map(this AuthorDto source) => null;
+    }
+}
---
+++ src/Inshapardaz.Database/Repositories/AuthorsRepository.cs
@@ -0,0 +1,62 @@
+﻿using Inshapardaz.Database;
+using Inshapardaz.Database.Dto.Library;
+using Inshapardaz.Domain.Entities;
+using Inshapardaz.Domain.Entities.Library;
+using Inshapardaz.Domain.Repositories.Library;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Inshapardaz.Database.Repositories
+{
+    public class AuthorsRepository : BaseRepository, IAuthorRepository
+    {
+        public Task<Author> AddAuthor(Author author, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAuthor(int authorId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Page<Author>> FindAuthors(string query, int pageNumber, int pageSize, CancellationToken cancellationToken)
+        {
+            var countQuery = @"SELECT * FROM Authors WHERE Name LIKE @Name";
+           
+            var totalCount = await ExecuteScalar<int>(countQuery, new { Name = query });
+
+            var sql = @"SELECT * FROM Authors 
+                        WHERE Name LIKE @Name
+                        OFFSET @PageNumber*@RowsPerPage ROWS
+                        FETCH NEXT @RowsPerPage ROWS ONLY";
+
+            var parameter = new
+            {
+                Name = query,
+                PageNumber = pageNumber,
+                RowsPerPage = pageSize
+            };
+
+            var authors = await QueryAsync<AuthorDto>(sql, parameter);
+
+            return authors.Map(pageNumber, pageSize, totalCount);
+        }
+
+        public Task<Author> GetAuthorById(int authorId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Page<Author>> GetAuthors(int pageNumber, int pageSize, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAuthor(Author author, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

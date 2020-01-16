795252295adaee97b4e147a848521bf889afeb50
Omar Faruk <omar.faruk@outlook.com>
2020-01-16 22:23:28 +0000

    Added dictionary Functions

---
+++ src/Inshapardaz.Functions/Dictionary/Words/Details/GetWordDetailsByWord.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Inshapardaz.Functions.Views;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Paramore.Brighter;
+
+namespace Inshapardaz.Functions.Dictionary.Words.Details
+{
+    public class GetWordDetailsByWord : FunctionBase
+    {
+        public GetWordDetailsByWord(IAmACommandProcessor commandProcessor)
+            : base(commandProcessor)
+        {
+        }
+
+        [FunctionName("GetWordDetailsByWord")]
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "dictionaries/{dictionaryId:int}/words/{wordId:long}/details")] HttpRequest req,
+            int dictionaryId, long wordId,
+            ILogger log)
+        {
+            return new OkObjectResult($"GET:GetWordDetailsByWord({dictionaryId}, {wordId})");            
+        }
+    }
+}
---
+++ src/Inshapardaz.Functions/Dictionary/Words/Details/UpdateWordDetail.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Inshapardaz.Functions.Views;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Paramore.Brighter;
+
+namespace Inshapardaz.Functions.Dictionary.Words.Details
+{
+    public class UpdateWordDetail : FunctionBase
+    {
+        public UpdateWordDetail(IAmACommandProcessor commandProcessor)
+            : base(commandProcessor)
+        {
+        }
+
+        [FunctionName("UpdateWordDetail")]
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "dictionaries/{dictionaryId:int}/words/{wordId:long}/details/{detailId:int}")] HttpRequest req,
+            int dictionaryId, long wordId, int detailId,
+            ILogger log)
+        {
+            return new OkObjectResult($"PUT:UpdateWordDetail({dictionaryId}, {wordId}, {detailId})");            
+        }
+    }
+}

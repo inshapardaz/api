af6b3cb05867194062a3e8c30b80b1d630898453
Omar Faruk <omar.faruk@outlook.com>
2020-10-12 12:50:53 +0100

    Added config method

--- src/Inshapardaz.Api/Controllers/HomeController.cs
+++ src/Inshapardaz.Api/Controllers/HomeController.cs
@@ -1,14 +1,29 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Inshapardaz.Domain.Adapters;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Inshapardaz.Api.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Settings _setting;
+
+        public HomeController(Settings setting)
+        {
+            _setting = setting;
+        }
+
         [HttpGet("health/check")]
         public IActionResult GetHeathCheck()
         {
             return Ok();
+        }
+
+        [HttpGet("health/config")]
+        public IActionResult GetConfig()
+        {
+            return Ok(JsonConvert.SerializeObject(_setting));
         }
     }
 }

567be5b8b690aba668f60cad9e93fe99b09e4a71
Omar Faruk <omar.faruk@outlook.com>
2020-10-12 13:07:50 +0100

    reverted test endpoint

--- src/Inshapardaz.Api/Controllers/HomeController.cs
+++ src/Inshapardaz.Api/Controllers/HomeController.cs
@@ -1,29 +1,13 @@
-﻿using Inshapardaz.Domain.Adapters;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Inshapardaz.Api.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly Settings _setting;
-
-        public HomeController(Settings setting)
-        {
-            _setting = setting;
-        }
-
         [HttpGet("health/check")]
         public IActionResult GetHeathCheck()
         {
             return Ok();
-        }
-
-        [HttpGet("health/config")]
-        public IActionResult GetConfig()
-        {
-            return Ok(JsonConvert.SerializeObject(_setting));
         }
     }
 }

1ede0672fb8a8697c83361717a34027685cde17f
Omar Faruk <omar.faruk@outlook.com>
2021-01-27 18:24:29 +0000

    Added security cookie specification

--- src/Inshapardaz.Api/Controllers/AccountsController.cs
+++ src/Inshapardaz.Api/Controllers/AccountsController.cs
@@ -179,7 +179,9 @@ namespace Inshapardaz.Api.Controllers
             var cookieOptions = new CookieOptions
             {
                 HttpOnly = true,
-                Expires = DateTime.UtcNow.AddDays(7)
+                Expires = DateTime.UtcNow.AddDays(7),
+                SameSite = SameSiteMode.None,
+                Secure = true
             };
             Response.Cookies.Append("refreshToken", token, cookieOptions);
         }

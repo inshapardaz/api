08c9ce224c2ffa0093ce76f81db50cd5c1af41c2
Omar Faruk <omar.faruk@outlook.com>
2021-06-21 22:34:56 +0100

    Added corretions

---
+++ src/Inshapardaz.Domain/Adapters/Repositories/ICorrectionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Inshapardaz.Domain.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Inshapardaz.Domain.Adapters.Repositories
+{
+    public interface ICorrectionRepository
+    {
+        Task<IEnumerable<Correction>> GetCorrectionForLanguage(string language, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Inshapardaz.Domain/Models/Correction.cs
@@ -0,0 +1,10 @@
+﻿namespace Inshapardaz.Domain.Models
+{
+    public class Correction
+    {
+        public int Id { get; set; }
+        public string Phrase { get; set; }
+        public string Replacement { get; set; }
+        public string Language { get; set; }
+    }
+}

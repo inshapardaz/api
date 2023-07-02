85b30ff86a35aa39d1fc90bc3cac0ddaca64a7ad
Omar Faruk <omar.faruk@outlook.com>
2023-07-02 07:50:50 +0100

    Fixed error creating author

--- src/Inshapardaz.Api/Views/Library/AuthorView.cs
+++ src/Inshapardaz.Api/Views/Library/AuthorView.cs
@@ -5,13 +5,9 @@ namespace Inshapardaz.Api.Views.Library
     public class AuthorView : ViewWithLinks
     {
         public int Id { get; set; }
-
-        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
         public int BookCount { get; set; }
-
-        [Required]
         public string AuthorType { get; set; }
     }
 }

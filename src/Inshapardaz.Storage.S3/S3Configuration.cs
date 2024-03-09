4746bb4e5f726ba37e939a05c6cf7a0e0fd5e808
Omar Faruk <omar.faruk@outlook.com>
2024-03-09 11:05:42 +0000

    Added library folder name for S3 storage

--- src/Inshapardaz.Storage.S3/S3Configuration.cs
+++ src/Inshapardaz.Storage.S3/S3Configuration.cs
@@ -6,5 +6,6 @@
         public string AccessSecret { get; set; }
         public string ServiceUrl { get; set; }
         public string BucketName { get; set; }
+        public string FolderName { get; set; }
 }
 }
